80b3fb7469e81be3dd2311c5a39839d7b563b76b
nozbi <76044507+nozbi@users.noreply.github.com>
2021-10-22 14:16:26 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -7,48 +7,15 @@
 <html>
     <head>
         <link rel="stylesheet" href="style.css">  
-    </head> 
-    
+    </head>   
     <body>
         <form method="post" action="login_validation.php" class="Form">
             <div class="Header">Username:</div>
-            <?php
-                if(isset($_SESSION['username']))
-                {
-                    echo'<input type="text" name="username" value="'.$_SESSION['username'].'" class="Input"></input>';
-                    unset($_SESSION['username']);
-                }
-                else
-                {
-                    echo'<input type="text" name="username" class="Input"></input>';
-                }
-            ?>
-            <?php
-                 if(isset($_SESSION['error_username']))
-                 {
-                     echo $_SESSION['error_username'];
-                     unset($_SESSION['error_username']);
-                 }
-            ?>
+            <input type="text" name="username" class="Input" value="<?php echo @$_SESSION['username']; unset($_SESSION['username']);?>"></input>
+            <?php echo @$_SESSION['error_username']; unset($_SESSION['error_username']); ?>
             <div class="Header">Password:</div>
-            <?php
-                if(isset($_SESSION['password']))
-                {
-                    echo'<input type="password" name="password" value="'.$_SESSION['password'].'" class="Input"></input>';
-                    unset($_SESSION['password']);
-                }
-                else
-                {
-                    echo'<input type="password" name="password" class="Input"></input>';
-                }
-            ?>
-            <?php
-                if(isset($_SESSION['error_password']))
-                {
-                    echo $_SESSION['error_password'];
-                    unset($_SESSION['error_password']);
-                }
-            ?>
+            <input type="password" name="password" class="Input" value="<?php echo @$_SESSION['password']; unset($_SESSION['password']);?>"></input>
+            <?php echo @$_SESSION['error_password']; unset($_SESSION['error_password']); ?>
             <input type="submit" value="Login" class="Button"></input>
             <input type="button" onclick="window.location.href='register.php';" value="Register" class="Button"></input>
         </form>    
